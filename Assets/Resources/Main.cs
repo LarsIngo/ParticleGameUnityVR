c0fa98b230ae74b449e2fd49b79d281889796493
SAJDCOMP\Pontus <pontus.bjorkberg@hotmail.com>
2017-07-16 17:59:26 +0200

    Righthand attractor

--- Assets/Resources/Main.cs
+++ Assets/Resources/Main.cs
@@ -7,23 +7,26 @@ public class Main : MonoBehaviour
 {
 
     GameObject mGO;
+    public GameObject attractor;
 
     private void Start ()
     {
         mGO = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-        mGO.transform.position = new Vector3(0,0,5);
+        mGO.transform.position = new Vector3(0,0,0);
+        mGO.transform.localScale *= 0.1f;
         GPUParticleSystem system = mGO.AddComponent<GPUParticleSystem>();
         system.EmittMesh = mGO.GetComponent<MeshFilter>().mesh;
         system.EmittParticleLifeTime = 30.0f;
         system.EmittFrequency = 500.0f;
         system.EmittInitialVelocity = new Vector3(0.0f, 0.0f, 0.0f);
-        system.EmittInitialScale = new Vector2(0.1f, 0.1f);
+        system.EmittInitialScale = new Vector2(0.01f, 0.01f);
         system.EmittInitialColor = new Vector3(0.0f, 1.0f, 0.0f);
+        system.TMPAcceleratorPower = 10f;
     }
 
     private void Update()
     {
-
+        mGO.GetComponent<GPUParticleSystem>().TMPAcceleratorPosition = attractor.transform.position;
     }
 
     private void OnDestroy()
