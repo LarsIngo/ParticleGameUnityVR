2314b3bceaf24a66ae8ffc4ad58d6f6da9197655
Birchmountain <pontus.bjorkberg@hotmail.com>
2017-07-17 16:51:06 +0200

    Fixed input on VR station

--- Assets/Resources/Main.cs
+++ Assets/Resources/Main.cs
@@ -9,14 +9,12 @@ public class Main : MonoBehaviour
     GameObject mParticleSystem;
 
     GameObject mHMD;
-    GameObject mLeftController;
-    GameObject mRightController;
+    public GameObject mLeftController;
+    public GameObject mRightController;
 
     private void Start ()
     {
         mHMD = GameObject.Find("Camera (head)");
-        mLeftController = GameObject.Find("Controller (left)");
-        mRightController = GameObject.Find("Controller (right)");
 
         if (mLeftController == null)
         {
@@ -35,27 +33,7 @@ public class Main : MonoBehaviour
 
     private void Update()
     {
-        SteamVR_TrackedController left = mLeftController.GetComponent<SteamVR_TrackedController>();
-        SteamVR_TrackedController right = mRightController.GetComponent<SteamVR_TrackedController>();
 
-        if (left != null)
-        {
-            SteamVR_Controller.Device device = SteamVR_Controller.Input((int)left.controllerIndex);
-            
-            if (left.triggerPressed)
-                left.GetComponent<GPUParticleVectorField>().Vector = Vector3.up * 20.0f;
-            else left.GetComponent<GPUParticleVectorField>().Vector = Vector3.up * 0.0f;
-
-        }
-        if(right != null)
-        {
-            SteamVR_Controller.Device device = SteamVR_Controller.Input((int)right.controllerIndex);
-
-            if(right.triggerPressed)
-                right.GetComponent<GPUParticleAttractor>().Power = 20.0f;
-            else right.GetComponent<GPUParticleAttractor>().Power = 0.0f;
-
-        }
     }
 
     private void OnDestroy()
