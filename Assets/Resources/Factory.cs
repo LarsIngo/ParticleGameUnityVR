8c18a39b4b9ba899d78f828f7fa17cced0a6d757
Trimblewick <gustav.ekelund@hotmail.com>
2017-07-18 14:46:55 +0200

    Added lifetimer

--- Assets/Resources/Factory.cs
+++ Assets/Resources/Factory.cs
@@ -8,8 +8,17 @@ public static class Factory {
     {
         GameObject michael = new GameObject();
         michael.transform.position = t.position;
+        michael.transform.rotation = t.rotation;
+
         GeometryExplosion exp = michael.AddComponent<GeometryExplosion>();
         exp.Mesh = mesh;
+
+        GPUParticleAttractor attractor = michael.AddComponent<GPUParticleAttractor>();
+        attractor.Power = 10.0f;
+
+        LifeTimer lifetimer = michael.AddComponent<LifeTimer>();
+        lifetimer.LifeTime = 4.0f;
+
 
         exp.Explode();
 
