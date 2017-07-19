ddd4d233844a728adcc51177405d7a1f9be2eacc
LarsIngo <ivarbjorling@hotmail.com>
2017-07-19 15:36:46 +0200

    Init timestretcher

--- Assets/Resources/Game/Entities/TimerStretcher.cs
+++ Assets/Resources/Game/Entities/TimerStretcher.cs
@@ -1,17 +1,56 @@
-﻿//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-//public class TimerStretcher : MonoBehaviour
-//{
+public class TimerStretcher : MonoBehaviour
+{
 
-//	void Awake ()
-//    {
-		
-//	}
+    float mTimer = 0.0f;
 
-//	void Update ()
-//    {
-		
-//	}
-//}
+    float mTimeIn = 0.0f;
+    float mTimeMain = 0.5f;
+    float mTimeOut = 0.5f;
+
+    float mTimeScale = 0.1f;
+
+	void Awake ()
+    {
+        Time.timeScale = mTimeScale;
+    }
+
+	void Update ()
+    {
+        mTimer += (Time.deltaTime / Time.timeScale);
+
+        float totalTime = mTimeIn + mTimeMain + mTimeOut;
+
+        if (mTimer < mTimeIn)
+        {
+            // IN.
+            float lerpFactor = mTimer / mTimeIn; // [0,1]
+            float slerpFactor = (1.0f - Mathf.Cos(lerpFactor * Mathf.PI)) / 2.0f; //[0,1]
+            float timeScale = mTimeScale + slerpFactor * (1.0f - mTimeScale);
+            Time.timeScale = Mathf.Clamp(1.0f - timeScale, mTimeScale, 1);
+        }
+        else if (mTimer < mTimeIn + mTimeMain)
+        {
+            // MAIN.
+            Time.timeScale = mTimeScale;
+        }
+        else
+        {
+            // OUT.
+            float lerpFactor = Mathf.Clamp01((mTimer - mTimeIn - mTimeMain) / mTimeOut); // [0,1]
+            float slerpFactor = (1.0f - Mathf.Cos(lerpFactor * Mathf.PI)) / 2.0f; // [0,1]
+
+            float timeScale = mTimeScale + slerpFactor * (1.0f - mTimeScale); // []
+            Time.timeScale = Mathf.Clamp(timeScale, mTimeScale, 1); ;
+        }
+
+        if (mTimer > totalTime)
+        {
+            Time.timeScale = 1.0f;
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Resources/Game/Factory.cs
+++ Assets/Resources/Game/Factory.cs
@@ -13,38 +13,39 @@ public static class Factory
     public static GameObject CreateMichaelBayEffect(Level level, Mesh mesh, Transform t, Color meshColor)
     {
         GameObject michael = level.CreateGameObject("michael" + Time.time);
-        GameObject blackHole = level.CreateGameObject("blackhole" + Time.time);
+        //GameObject blackHole = level.CreateGameObject("blackhole" + Time.time);
         michael.transform.position = t.position;
         michael.transform.rotation = t.rotation;
-        blackHole.transform.position = t.position;
-        blackHole.transform.rotation = t.rotation;
+        //blackHole.transform.position = t.position;
+        //blackHole.transform.rotation = t.rotation;
 
         GeometryExplosion exp = michael.AddComponent<GeometryExplosion>();
         exp.Mesh = mesh;
         exp.ExplosionColor = meshColor;
+        exp.ExplosionSpeed = 5;
 
         LifeTimer michaelLifetimer = michael.AddComponent<LifeTimer>();
-        michaelLifetimer.LifeTime = 1.2f;
+        michaelLifetimer.LifeTime = 4.0f;
 
-        GPUParticleAttractor attractor = blackHole.AddComponent<GPUParticleAttractor>();
-        attractor.Power = 10.0f;
+        //GPUParticleAttractor attractor = blackHole.AddComponent<GPUParticleAttractor>();
+        //attractor.Power = 10.0f;
 
-        LifeTimer blackHoleLifetimer = blackHole.AddComponent<LifeTimer>();
-        blackHoleLifetimer.LifeTime = 4.0f;
+        //LifeTimer blackHoleLifetimer = blackHole.AddComponent<LifeTimer>();
+        //blackHoleLifetimer.LifeTime = 4.0f;
 
-        GPUParticleSystem particles = blackHole.AddComponent<GPUParticleSystem>();
-        particles.EmittParticleLifeTime = 7.0f;
-        particles.EmittFrequency = 50.0f;
+        //GPUParticleSystem particles = blackHole.AddComponent<GPUParticleSystem>();
+        //particles.EmittParticleLifeTime = 7.0f;
+        //particles.EmittFrequency = 50.0f;
 
-        Vector4[] colorLife = new Vector4[2];
-        colorLife[0] = meshColor;
-        colorLife[1] = new Vector4(1.0f, 1.0f, 1.0f, 0.0f);
-        particles.ColorLifetimePoints = colorLife;
+        //Vector4[] colorLife = new Vector4[2];
+        //colorLife[0] = new Vector4(meshColor.r, meshColor.g, meshColor.b, 0.0f); ;
+        //colorLife[1] = new Vector4(1.0f, 1.0f, 1.0f, 1.0f);
+        //particles.ColorLifetimePoints = colorLife;
 
-        Vector4[] scaleLife = new Vector4[2];
-        scaleLife[0] = new Vector4(0.01f, 0.01f, 0.0f, 0.0f);
-        scaleLife[1] = new Vector4(0.001f, 0.001f, 0.0f, 1.0f);
-        particles.ScaleLifetimePoints = scaleLife;
+        //Vector4[] scaleLife = new Vector4[2];
+        //scaleLife[0] = new Vector4(0.01f, 0.01f, 0.0f, 0.0f);
+        //scaleLife[1] = new Vector4(0.001f, 0.001f, 0.0f, 1.0f);
+        //particles.ScaleLifetimePoints = scaleLife;
 
         exp.Explode();
 
