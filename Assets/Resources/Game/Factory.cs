974276658573ed66ddd929e62fbad1543c050ce8
LarsIngo <ivarbjorling@hotmail.com>
2017-07-21 22:14:26 +0200

    .0f

--- Assets/Resources/Game/Factory.cs
+++ Assets/Resources/Game/Factory.cs
@@ -96,7 +96,7 @@ public static class Factory
         // FireworkSpawn
         GameObject fireworkSpawn = new GameObject("FireworkSpawn " + Time.time);
 
-        fireworkSpawn.AddComponent<FireworkSpawn>().mFrequency = 2.f;
+        fireworkSpawn.AddComponent<FireworkSpawn>().mFrequency = 2.0f;
 
         fireworkSpawn.AddComponent<LifeTimer>().LifeTime = lifetime / 2.0f;
     }
